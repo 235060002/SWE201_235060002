472f8fb4e621127f9559ad12210cf9f2de00cf45
Aday AKYÜZ <235060002@toros.edu.tr>
2025-11-27 18:14:56 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,45 @@
+﻿public class Car
+{
+    // Member variables
+    private string _brand;
+    private int _year;
+
+    // Properties
+    public string Brand
+    {
+        get { return _brand; }
+        set { _brand = value; }
+    }
+
+    public int Year
+    {
+        get { return _year; }
+        set { _year = value; }
+    }
+
+    // Empty constructor
+    public Car()
+    {
+        _brand = "Unknown";
+        _year = 0;
+    }
+
+    // Constructor with parameters
+    public Car(string brand, int year)
+    {
+        _brand = brand;
+        _year = year;
+    }
+
+    // Member function
+    public int GetCarAge()
+    {
+        return DateTime.Now.Year - _year;
+    }
+
+    // Override of ToString()
+    public override string ToString()
+    {
+        return $"{_brand} - {_year}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Object with empty constructor
+        Car c1 = new Car();
+        Console.WriteLine("Default car:");
+        Console.WriteLine(c1.ToString());
+        Console.WriteLine();
+
+        // Object with parameterized constructor
+        Car c2 = new Car("Toyota", 2018);
+        Console.WriteLine("Custom car:");
+        Console.WriteLine(c2.ToString());
+        Console.WriteLine("Car age: " + c2.GetCarAge());
+        Console.WriteLine();
+
+        // Updating values using properties
+        c1.Brand = "BMW";
+        c1.Year = 2020;
+
+        Console.WriteLine("Updated first car:");
+        Console.WriteLine(c1.ToString());
+        Console.WriteLine("Car age: " + c1.GetCarAge());
+    }
+}
